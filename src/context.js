4ddc852cb5acadfc758426632384c32d8c3ee5e8
maryna <merry.kosiv@gmail.com>
2020-12-10 17:58:02 +0200

    initial commit

---
+++ src/context.js
@@ -0,0 +1,65 @@
+import React, {useState, useContext, useEffect} from 'react'
+import {useCallback} from 'react'
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [cocktails, setCocktails] = useState([]);
+
+
+    const fetchDrinks = useCallback(async () => {
+        setLoading(true);
+
+        try {
+            const response = await fetch(`${url}${searchTerm}`);
+            const data = await response.json();
+            const {drinks} = data;
+
+            if(drinks) {
+                const newCocktails = drinks.map((drink) => {
+                    const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = drink;
+                    return {
+                        id: idDrink,
+                        name: strDrink,
+                        img: strDrinkThumb,
+                        info: strAlcoholic,
+                        glass: strGlass,
+                    }
+                });
+                setCocktails(newCocktails);
+            }
+
+        else {
+                setCocktails([])
+            }
+            setLoading(false);
+        } catch (e) {
+            console.log(e);
+            setLoading(false);
+        }
+    }, [searchTerm]);
+    useEffect(() => {
+        fetchDrinks()
+    }, [searchTerm, fetchDrinks]);
+
+    return (
+        <AppContext.Provider
+            value={{
+                loading,
+                cocktails,
+                setSearchTerm,
+            }}>
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+};
+
+export {AppContext, AppProvider}
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='section about-section'>
+            <h1 className='section-title'>About Us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, esse id minus quas qui ullam voluptatum. Aut dicta dolorem eos fuga modi? A distinctio doloremque eaque eum, quam quibusdam sed. Enim error facere labore nam, optio sed sequi ullam veniam.</p>
+        </section>
+    )
+};
+
+export default About
